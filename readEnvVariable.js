f6afe4cf9457b391e1ca2902f4061022fea0db68
PendemSwetha <swetha05p@gmail.com>
2022-12-13 20:24:48 +0530

    added

---
+++ readEnvVariable.js
@@ -0,0 +1,3 @@
+
+console.log("Hello",process.env.USERNAME);
+// console.log(process)
---
+++ readFromLine.js
@@ -0,0 +1,12 @@
+const readline=require("readline");
+
+const readL=readline.createInterface(
+    {
+        input:process.stdin,
+        output:process.stdout
+    }
+)
+readL.question("Please enter your name:",(ans)=>{
+    console.log("Hello", ans);
+    readL.close();
+})
